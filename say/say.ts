90bee8d3650ec03e0963fd2c515c8d610293c68a
CloudRex <private.phantom@outlook.com>
2018-10-14 18:02:28 -0400

    Added the say command

---
+++ say/say.ts
@@ -0,0 +1,42 @@
+import {Argument, Command, PrimitiveArgType, CommandContext} from "forge";
+
+type SayArgs = {
+    readonly message: string;
+}
+
+const mentionPattern: RegExp = /<@!?[0-9]+>|@everyone|@here/gm;
+
+export default class EmulateCommand extends Command {
+    readonly meta = {
+        name: "say",
+        description: "Send a message"
+    };
+
+    readonly aliases = ["echo"];
+
+    readonly arguments: Argument[] = [
+        {
+            name: "message",
+            description: "The message to send",
+            type: PrimitiveArgType.String,
+            required: true
+        }
+    ];
+
+    constructor() {
+        super();
+
+        //this.restrict.ownerOnly = true;
+    }
+
+    public async executed(context: CommandContext, args: SayArgs): Promise<void> {
+        let filteredMessage: string = args.message;
+
+        while (mentionPattern.test(filteredMessage)) {
+            filteredMessage = filteredMessage.replace(mentionPattern, "[Mention]");
+        }
+
+        // TODO: Debugging
+        await context.message.channel.send(`${context.sender.tag} <:announcement:490726045880811531> ` + filteredMessage);
+    }
+};
