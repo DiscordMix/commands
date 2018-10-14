f502549b913aae4bc268873fdd1097a937d56745
CloudRex <private.phantom@outlook.com>
2018-10-14 19:57:54 -0400

    Added the test command

---
+++ test/test.ts
@@ -0,0 +1,34 @@
+import {Command, CommandContext, RestrictGroup} from "forge";
+import {TextChannel, Permissions} from "discord.js";
+
+export default class TestCommand extends Command {
+    readonly meta = {
+        name: "test",
+        description: "Test whether I can send messages in this channel"
+    };
+
+    readonly restrict: any = {
+        specific: [RestrictGroup.ServerModerator]
+    };
+
+    public async executed(context: CommandContext): Promise<void> {
+        // TODO: Check if the send message failed and display error, and check if embeds + files can be sent.
+        if (context.message.channel.type === "text") {
+            const channel: TextChannel = context.message.channel as TextChannel;
+            const permissions: Permissions | null = await channel.permissionsFor(context.message.guild.me);
+
+            let canEmbed: boolean = context.message.guild.me.hasPermission("EMBED_LINKS");
+
+            if (permissions !== null) {
+                canEmbed = permissions.hasPermission("EMBED_LINKS");
+            }
+
+            if (canEmbed) {
+                await context.ok("I can send messages in this channel");
+            }
+            else {
+                await channel.send("I'm missing the `EMBED_LINKS` permission required for embeds");
+            }
+        }
+    }
+};
