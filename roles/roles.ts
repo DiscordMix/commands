b70b6122e1a4425957bc7de7e79e1c2cf9a1bb9a
CloudRex <cloudrex@outlook.com>
2018-10-15 12:13:57 -0400

    Added roles command

---
+++ roles/roles.ts
@@ -0,0 +1,34 @@
+import {Role} from "discord.js";
+import {ChatEnvironment, Command, Argument, CommandContext, RestrictGroup, PrimitiveArgType} from "forge";
+
+type RolesArgs = {
+    readonly page: number;
+}
+
+export default class RolesCommand extends Command {
+    readonly meta = {
+        name: "roles",
+        description: "Display the server's roles",
+    };
+
+    readonly arguments: Argument[] = [
+        {
+            name: "page",
+            type: PrimitiveArgType.UnsignedInteger,
+            defaultValue: 0
+        }
+    ];
+
+    readonly restrict: any = {
+        specific: [RestrictGroup.ServerModerator],
+        environment: ChatEnvironment.Guild
+    };
+
+    public executed(context: CommandContext, args: RolesArgs): void {
+        context.ok(context.message.guild.roles.array()
+            .map((role: Role) => `<@&${role.id}> => ${role.id}`)
+            .join("\n")
+            .substring(args.page * 2048)
+            .substr(0, 2048));
+    }
+};
