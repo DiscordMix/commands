db6e07edc813181667f4df5700e59904e996302a
CloudRex <private.phantom@outlook.com>
2018-10-14 20:12:12 -0400

    Added info command definition

--- info/info.ts
+++ info/info.ts
@@ -1,4 +1,4 @@
-import {Command, CommandContext, Permission, Utils} from "forge";
+import {Command, CommandContext, Utils} from "forge";
 import {Emoji, RichEmbed} from "discord.js";
 
 // TODO: Bot should have a command to display info of itself, ex. uptime.
