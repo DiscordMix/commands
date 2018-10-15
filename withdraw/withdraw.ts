03abda7e555811c787a971fdb09d4a8b7fdc506f
CloudRex <cloudrex@outlook.com>
2018-10-15 13:19:28 -0400

    Added the withdraw command

---
+++ withdraw/withdraw.ts
@@ -0,0 +1,19 @@
+import {Command, CommandContext, RestrictGroup} from "forge";
+
+export default class Withdraw extends Command {
+    readonly meta = {
+        name: "withdraw",
+        description: "Leave the current server"
+    };
+
+    readonly aliases = ["leave"];
+
+    readonly restrict: any = {
+        specific: [RestrictGroup.BotOwner]
+    };
+
+	public async executed(context: CommandContext): Promise<void> {
+		await context.ok(":ok_hand: Bye!");
+		await context.message.guild.leave();
+	}
+};
